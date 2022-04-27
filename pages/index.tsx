acbc88729aec92694060f12a09d0b36e824d23f4
Kevin Yu <kevinyu567@gmail.com>
2022-04-26 19:05:03 -0700

    Add `_document`, initial styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,8 +10,10 @@ export default function Home() {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
 
-            <main>
-                <h1 className="text-7xl">Stanley Zhong is cute.</h1>
+            <main className="h-screen flex items-center justify-center">
+                <h1 className="text-7xl text-center font-bold bg-gradient-to-r from-pink-500 to-red-600 bg-clip-text text-transparent pb-4">
+                    Stanley Zhong is cute.
+                </h1>
             </main>
         </div>
     )
