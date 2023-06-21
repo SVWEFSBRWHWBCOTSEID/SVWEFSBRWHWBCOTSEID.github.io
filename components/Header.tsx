5ff96afa87ef2a928853e659a336b705a062b275
Kevin Yu <kevinyu567@gmail.com>
2023-06-20 18:14:30 -0700

    Add more placeholders to header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,14 +1,25 @@
 import {ReactNode} from 'react';
 import Link from 'next/link';
+import {BsGear, BsGearFill} from 'react-icons/bs';
 
 
 export default function Header() {
     return (
-        <header className="px-8 py-4 text-md flex gap-2 items-center">
-            <Link href="/" className="text-xl pb-0.5 mr-4">[game website]</Link>
+        <header className="px-8 py-4 text-md flex justify-between">
+            <nav className="flex gap-2 items-center">
+                <Link href="/" className="text-xl pb-0.5 mr-4">[game website]</Link>
 
-            <HeaderLink href="/offline">Offline</HeaderLink>
-            <HeaderLink href="/rules">Rules</HeaderLink>
+                <HeaderLink href="/offline">Offline</HeaderLink>
+                <HeaderLink href="/rules">Rules</HeaderLink>
+            </nav>
+            <nav className="flex gap-2 items-center">
+                <Link href="/preferences">
+                    <BsGearFill className="text-secondary" />
+                </Link>
+                <Link href="/profile" className="text-secondary px-2">
+                    qpwoeirut
+                </Link>
+            </nav>
         </header>
     )
 }
