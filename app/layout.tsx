cac6bbf688fef2aa9d8118301faa738b13bce130
Kevin Yu <kevinyu567@gmail.com>
2023-06-20 17:39:14 -0700

    Use inter, update header spacing

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,14 @@
 import {ReactNode} from 'react';
+import {Inter} from 'next/font/google';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 import '../styles/styles.css'
 
+
+const inter = Inter({
+    subsets: ['latin']
+});
 
 export default function Layout(props: {children: ReactNode}) {
     return (
@@ -12,7 +17,7 @@ export default function Layout(props: {children: ReactNode}) {
                 <meta charSet="utf-8" />
                 <link rel="icon" href="/favicon.ico" />
             </head>
-            <body className="bg-zinc-800 text-white h-full flex flex-col">
+            <body className="bg-zinc-800 text-white h-full flex flex-col" style={inter.style}>
                 <Header />
                 {props.children}
             </body>
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,22 @@
+import {ReactNode} from 'react';
 import Link from 'next/link';
+
 
 export default function Header() {
     return (
-        <header className="px-8 py-4 text-lg flex gap-4 items-center">
-            <Link href="/" className="text-xl">[game website]</Link>
-            <Link href="/offline" className="uppercase">Offline</Link>
-            <Link href="/rules" className="uppercase">Rules</Link>
+        <header className="px-8 py-4 text-md flex gap-4 items-center">
+            <Link href="/" className="text-xl pb-0.5">[game website]</Link>
+
+            <HeaderLink href="/offline">Offline</HeaderLink>
+            <HeaderLink href="/rules">Rules</HeaderLink>
         </header>
+    )
+}
+
+function HeaderLink(props: {href: string, children: ReactNode}) {
+    return (
+        <Link href={props.href} className="uppercase">
+            {props.children}
+        </Link>
     )
 }
