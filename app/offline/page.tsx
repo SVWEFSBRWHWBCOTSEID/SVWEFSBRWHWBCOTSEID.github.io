f216bd56fcee023989e9286495de15fed9673c30
Kevin Yu <kevinyu567@gmail.com>
2023-06-20 17:25:45 -0700

    Add offline to header, update offline page

--- app/offline/page.tsx
+++ app/offline/page.tsx
@@ -1,51 +1,47 @@
 import {ReactNode} from 'react';
-import Head from 'next/head';
+import {Metadata} from 'next';
 import Link from 'next/link';
 
 
+export const metadata: Metadata = {
+    title: 'Offline Games',
+    description: 'Locally hosted games that don\'t require a server connection to play.'
+}
+
 export default function Home() {
     return (
-        <div>
-            <Head>
-                <title>Offline Games</title>
-                <meta name="description" content="Rabbit sign." />
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
+        <div className="mt-24">
+            <h1 className="text-7xl text-center font-bold mb-4">
+                Offline games
+            </h1>
+            <p className="text-center mb-8">
+                Locally hosted games that don't require a server connection to play.
+            </p>
 
-            <main className="h-screen flex flex-col items-center justify-center">
-                <h1 className="text-7xl text-center font-bold mb-4">
-                    Offline games
-                </h1>
-                <p className="mb-8">
-                    Locally hosted games that don't require a server connection to play.
-                </p>
+            <section className="flex flex-wrap justify-center gap-6">
+                <GameCard href="/offline/ttt">
+                    <GameCardHeading>Tic-Tac-Toe</GameCardHeading>
+                    <img src="/ttt.png" alt="ttt" className="w-72" />
+                    <p>...</p>
+                </GameCard>
 
-                <section className="flex flex-wrap justify-center gap-6">
-                    <GameCard href="/offline/ttt">
-                        <GameCardHeading>Tic-Tac-Toe</GameCardHeading>
-                        <img src="/ttt.png" alt="ttt" className="w-72" />
-                        <p>...</p>
-                    </GameCard>
-
-                    <GameCard href="/offline/uttt">
-                        <GameCardHeading>Ultimate Tic-Tac-Toe</GameCardHeading>
-                        <img src="/uttt.png" alt="uttt" className="w-72" />
-                        <p>...</p>
-                    </GameCard>
-                </section>
-            </main>
+                <GameCard href="/offline/uttt">
+                    <GameCardHeading>Ultimate Tic-Tac-Toe</GameCardHeading>
+                    <img src="/uttt.png" alt="uttt" className="w-72" />
+                    <p>...</p>
+                </GameCard>
+            </section>
         </div>
     )
 }
 
+// TODO: restyle these
 function GameCard(props: {href: string, children: ReactNode}) {
     return (
-        <Link href={props.href}>
-            <a className="flex">
-                <div className="group px-6 py-4 rounded-lg shadow-lg bg-black/30 hover:bg-black/50 transition duration-150">
-                    {props.children}
-                </div>
-            </a>
+        <Link href={props.href} className="flex">
+            <div className="group px-6 py-4 rounded-lg shadow-lg bg-black/30 hover:bg-black/50 transition duration-150">
+                {props.children}
+            </div>
         </Link>
     )
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -4,6 +4,7 @@ export default function Header() {
     return (
         <header className="px-8 py-4 text-lg flex gap-4 items-center">
             <Link href="/" className="text-xl">[game website]</Link>
+            <Link href="/offline" className="uppercase">Offline</Link>
             <Link href="/rules" className="uppercase">Rules</Link>
         </header>
     )
