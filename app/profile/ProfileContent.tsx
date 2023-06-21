65ebcd4128879da353ba0f106e4e37eecbae2ccf
Kevin Yu <kevinyu567@gmail.com>
2023-06-21 00:20:21 -0700

    Game panel placeholders, fix sidebar squishing

--- app/profile/ProfileContent.tsx
+++ app/profile/ProfileContent.tsx
@@ -9,13 +9,13 @@ import ProfileGamePanel from './ProfileGamePanel';
 // TODO: I wish that `use-client` didn't force this semi-hacky component wrapping, but alas
 export default function ProfileContent() {
     return (
-        <Tab.Group vertical as="div" className="container flex pt-4">
+        <Tab.Group vertical as="div" className="container flex pt-4 pb-12">
             <ProfileSidebar />
-            <Tab.Panels className="flex-grow bg-content rounded-t-lg overflow-clip">
+            <Tab.Panels className="flex-grow bg-content rounded-lg overflow-clip">
                 <ProfileUserPanel />
 
                 {Object.entries(ratings).map(([key, value]) => (
-                    <ProfileGamePanel key={key} />
+                    <ProfileGamePanel name={keyToName(key)} {...value} key={key} />
                 ))}
             </Tab.Panels>
         </Tab.Group>
--- app/profile/ProfileGamePanel.tsx
+++ app/profile/ProfileGamePanel.tsx
@@ -1,12 +1,43 @@
 'use client'
 
+import Link from 'next/link';
 import {Tab} from '@headlessui/react';
+import ProfileGames from './ProfileGames';
 
 
-export default function ProfileGamePanel() {
+type ProfileGamePanelProps = {name: string, rating: number, games: number, provisional?: boolean}
+export default function ProfileGamePanel(props: ProfileGamePanelProps) {
     return (
         <Tab.Panel>
-            [todo]
+            <h1 className="px-8 py-6 text-4xl">
+                <Link href="/profile" className="text-blue-500 hover:underline">kepler</Link>'s {props.name} stats
+            </h1>
+
+            <section className="border-y border-tertiary flex items-center justify-center text-secondary py-12">
+                [todo: lichess elo chart thing]
+            </section>
+
+            <section className="px-8 py-6">
+                <p className="text-2xl mb-3">
+                    Rating: <strong className="font-semibold">{props.rating}</strong>.{' '}
+                    {props.provisional ? (
+                        <span className="text-secondary">(provisional)</span>
+                    ) : (
+                        // TODO: links
+                        <span className="text-secondary">
+                            You are better than 58.7% of {props.name} players.
+                        </span>
+                    )}
+                </p>
+
+                {/* TODO: rating change arrow / color, tooltips */}
+                <p className="text-sm text-secondary">
+                    Progression over the last 12 games: -17. Rating deviation: 71.89.
+                </p>
+            </section>
+
+            {/* TODO: filter these by game */}
+            <ProfileGames />
         </Tab.Panel>
     )
 }
