425fd63d69bc3561da1fce737e34c619923ea814
Kevin Yu <kevinyu567@gmail.com>
2023-06-26 13:44:32 -0700

    Add hover to lobby rooms

--- components/LobbyRoom.tsx
+++ components/LobbyRoom.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 
 export default function LobbyRoom() {
     return (
-        <Link className="px-4 table-row text-secondary" href="/ttt/123">
+        <Link className="px-4 table-row text-secondary group" href="/ttt/123">
             <LobbyCell>Gunstable</LobbyCell>
             <LobbyCell>1537</LobbyCell>
             <LobbyCell>5+5</LobbyCell>
@@ -15,7 +15,7 @@ export default function LobbyRoom() {
 
 export function LobbyCell(props: {children: ReactNode}) {
     return (
-        <span className="table-cell border-t border-tertiary px-4 py-1">
+        <span className="table-cell border-t border-tertiary px-4 py-1 group-hover:text-white group-hover:bg-[rgb(214_79_0_/_0.5)]">
             {props.children}
         </span>
     )
