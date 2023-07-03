6c29283daf32f50107dbe962cdab50e405f95e4e
Kevin Yu <kevinyu567@gmail.com>
2023-07-02 18:31:56 -0700

    Use server rendering instead of middleware for auth blocking
    Mystical cache miss upon visiting the page, will need to investigate later.

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -3,6 +3,7 @@ import ProfileContent from './ProfileContent';
 import {getServerSession} from 'next-auth';
 import {authOptions} from '../api/auth/[...nextauth]/route';
 import {getUser} from '../../util/users';
+import {redirect} from 'next/navigation';
 
 
 export const metadata: Metadata = {
@@ -11,7 +12,7 @@ export const metadata: Metadata = {
 
 export default async function Profile() {
     const session = await getServerSession(authOptions);
-    if (!session?.user?.name) return; // TODO: use this instead of middleware?
+    if (!session?.user?.name) redirect('/login');
 
     const user = await getUser(session.user.name);
     if (!user) return;
