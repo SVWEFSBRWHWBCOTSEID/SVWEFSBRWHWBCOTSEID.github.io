2eb92668b26143c490ea1b77eba7b1a178ac7433
Kevin Yu <kevinyu567@gmail.com>
2023-07-09 00:03:54 -0700

    Move history header controls

--- app/game/GameMoveHistory.tsx
+++ app/game/GameMoveHistory.tsx
@@ -1,5 +1,6 @@
-import {Fragment, ReactNode} from 'react';
+import {Fragment, MouseEventHandler, ReactNode} from 'react';
 import {useHotkeys} from 'react-hotkeys-hook'
+import {MdFastForward, MdFastRewind, MdSkipNext, MdSkipPrevious} from 'react-icons/md';
 
 
 export type GameMoveHistoryProps = {moves: string[], index: number, setIndex: (i: number) => void}
@@ -11,8 +12,31 @@ export default function GameMoveHistory(props: GameMoveHistoryProps) {
 
     return (
         <>
-            <div className="bg-content-secondary px-5 py-1.5 border-b border-tertiary">
-                aaa
+            <div className="bg-content-secondary border-b border-tertiary flex justify-center text-secondary text-2xl">
+                <HeaderButton
+                    onClick={() => setIndex(1)}
+                    disabled={index === 1}
+                >
+                    <MdFastRewind />
+                </HeaderButton>
+                <HeaderButton
+                    onClick={() => setIndex(Math.max(index - 1, 1))}
+                    disabled={index === 1}
+                >
+                    <MdSkipPrevious />
+                </HeaderButton>
+                <HeaderButton
+                    onClick={() => setIndex(Math.min(index + 1, moves.length))}
+                    disabled={index === moves.length}
+                >
+                    <MdSkipNext />
+                </HeaderButton>
+                <HeaderButton
+                    onClick={() => setIndex(moves.length)}
+                    disabled={index === moves.length}
+                >
+                    <MdFastForward />
+                </HeaderButton>
             </div>
             <div className="flex-shrink overflow-auto mb-auto">
                 <div className="grid grid-cols-[4rem_1fr_1fr] h-max">
@@ -39,6 +63,18 @@ function GameMove(props: {index: number, currentIndex: number, setIndex: (i: num
         <button
             className={'px-4 py-1 text-sm ' + (active ? 'bg-[#293a49]' : 'hover:bg-blue-500')}
             onClick={() => props.setIndex(props.index)}
+        >
+            {props.children}
+        </button>
+    )
+}
+
+function HeaderButton(props: {onClick: MouseEventHandler<HTMLButtonElement>, disabled?: boolean, children: ReactNode}) {
+    return (
+        <button
+            className="px-4 py-1.5 hover:bg-theme-green hover:text-white transition duration-150 disabled:opacity-30 disabled:hover:text-inherit disabled:hover:bg-transparent"
+            onClick={props.onClick}
+            disabled={props.disabled}
         >
             {props.children}
         </button>
