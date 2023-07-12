b388b530d6da69d9641a8a7aa98f0cb6c1280e93
Kevin Yu <kevinyu567@gmail.com>
2023-07-12 15:48:55 -0700

    Configure prod backend URL

--- components/SignInLink.tsx
+++ components/SignInLink.tsx
@@ -6,7 +6,7 @@ import Link from 'next/link';
 export default function SignInLink() {
     return (
         <Link
-            href={`/login?callbackUrl=${encodeURIComponent(window?.location.href)}`}
+            href={`/login?callbackUrl=${encodeURIComponent(typeof window !== 'undefined' && window.location.href)}`}
             className="text-blue-500 uppercase px-3 py-4"
         >
             Sign in
