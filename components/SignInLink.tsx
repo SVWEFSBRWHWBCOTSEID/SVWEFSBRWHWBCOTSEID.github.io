b0bca53cc8e7bdc20a6b19092b2cbd1580cab23a
Kevin Yu <kevinyu567@gmail.com>
2023-07-22 23:28:47 -0700

    Hopefully fix callbackUrl on sign in link

--- components/SignInLink.tsx
+++ components/SignInLink.tsx
@@ -1,12 +1,18 @@
 'use client'
 
+import {useLayoutEffect, useState} from 'react';
 import Link from 'next/link';
 
 
 export default function SignInLink() {
+    const [url, setUrl] = useState('');
+    useLayoutEffect(() => {
+        setUrl(window.location.href)
+    }, [])
+
     return (
         <Link
-            href={`/login?callbackUrl=${encodeURIComponent(typeof window !== 'undefined' && window.location.href)}`}
+            href={`/login?callbackUrl=${encodeURIComponent(url)}`}
             className="text-blue-500 uppercase px-3 py-4"
         >
             Sign in
