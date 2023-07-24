22d9c50e0621594fb88c5651e62d782e23257eab
Kevin Yu <kevinyu567@gmail.com>
2023-07-23 22:04:13 -0700

    Join room on click

--- app/(home)/LobbyRoom.tsx
+++ app/(home)/LobbyRoom.tsx
@@ -1,17 +1,34 @@
-import {ReactNode} from 'react';
-import Link from 'next/link';
+'use client'
+
+import {ReactNode, useState} from 'react';
 import {timeControlToString} from '../../util/game';
 import type {Lobby} from './Lobbies';
 
 
 export default function LobbyRoom(props: Lobby) {
+    const [loading, setLoading] = useState(false)
+
+    async function joinGame() {
+        if (loading) return;
+        setLoading(true);
+
+        await fetch(`${process.env.API_BASE}/game/join/${props.id}`, {
+            method: 'POST',
+            credentials: 'include'
+        });
+    }
+
     return (
-        <Link className="px-4 table-row text-secondary group" href={`/game/${props.id}`}>
+        // TODO: button is not a legal table row element, but using a div is kinda hacky
+        <div
+            className={'px-4 table-row text-secondary group' + (loading ? ' opacity-50' : '')}
+            onClick={joinGame}
+        >
             <LobbyCell>{props.user.username}</LobbyCell>
             <LobbyCell>{props.user.rating}</LobbyCell>
             <LobbyCell>{timeControlToString(props.timeControl)}</LobbyCell>
             <LobbyCell>{props.game.key.toUpperCase()} {props.rated ? 'Rated' : 'Casual'}</LobbyCell>
-        </Link>
+        </div>
     )
 }
 
