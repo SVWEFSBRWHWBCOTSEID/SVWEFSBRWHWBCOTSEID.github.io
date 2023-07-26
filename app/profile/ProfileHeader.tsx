9fd15bbee0e55101611db71a50cf55a00436934f
Kevin Yu <kevinyu567@gmail.com>
2023-07-25 20:12:14 -0700

    Placeholder letter circle for no-pfp players

--- app/profile/ProfileHeader.tsx
+++ app/profile/ProfileHeader.tsx
@@ -10,14 +10,25 @@ export default function ProfileHeader() {
 
     return (
         <section className="flex gap-6 px-8 py-6 bg-content-secondary">
-            <img src="/pfp.png" className="w-16 h-16 rounded-full object-cover object-center" alt={username} />
+            {false ? (
+                // TODO
+                <img
+                    src="/pfp.png"
+                    className="w-16 h-16 rounded-full object-cover object-center"
+                    alt={username}
+                />
+            ) : (
+                <div className="w-16 h-16 rounded-full bg-background flex items-center justify-center text-secondary/50 text-3xl font-medium">
+                    {username[0].toUpperCase()}
+                </div>
+            )}
             <div className="pt-3">
                 <h1 className="text-4xl flex gap-3 items-center mb-3">
                     {username}
                     <img src="https://lichess1.org/assets/_zkgwWf/images/flags/US.png" alt="US flag" className="h-6" />
                 </h1>
                 <p className="text-secondary"><strong>Joined:</strong> {DateTime.fromSQL(createdAt).toLocaleString()}</p>
-                <p className="text-secondary"><strong>Followers:</strong> 0</p>{/* TODO: followers */}
+                <p className="text-secondary"><strong>Friends:</strong> 0</p>{/* TODO: followers */}
             </div>
         </section>
     )
