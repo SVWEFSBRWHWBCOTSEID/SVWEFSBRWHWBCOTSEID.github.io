4056c7173045bb4fe24b3bb80e746ecb9db882de
Kevin Yu <kevinyu567@gmail.com>
2023-09-24 23:03:31 -0400

    Use real links in profile games

--- app/profile/ProfileGame.tsx
+++ app/profile/ProfileGame.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import {ReactNode, useContext} from 'react';
-import {useRouter} from 'next/navigation';
+import Link from 'next/link';
 import {DateTime} from 'luxon';
 import {BiSolidPlusSquare, BiSolidMinusSquare} from 'react-icons/bi';
 import type {EndType, GameInfo, Status} from '../game/[id]/page';
@@ -18,28 +18,24 @@ export type ProfileGameInfo = GameInfo & {
 }
 export default function ProfileGame(props: ProfileGameInfo) {
     const Icon = keyToIcon(props.game.key);
-
-    // TODO: not valid as table row, but somehow use `<a>` or `<Link>`?
     const {username} = useContext(ProfileContext);
-    const {push} = useRouter();
+
+    const href = `/game/${props.id}`;
 
     return (
-        <div
-            className="table-row text-sm text-primary cursor-pointer hover:bg-blue-500/20"
-            onClick={() => push(`/game/${props.id}`)}
-        >
-            <ProfileGameCell>
+        <div className="table-row text-sm text-primary cursor-pointer hover:bg-blue-500/20">
+            <ProfileGameCell href={href}>
                 <Icon className="text-4xl" />
             </ProfileGameCell>
-            <ProfileGameCell>
+            <ProfileGameCell href={href}>
                 <strong className="font-medium">{props.first.username}</strong> ({props.first.rating})
                 <br />
                 <strong className="font-medium">{props.second.username}</strong> ({props.second.rating})
             </ProfileGameCell>
-            <ProfileGameCell>
+            <ProfileGameCell href={href}>
                 {timeControlToString(props.timeControl)} | {props.game.key.toUpperCase()}
             </ProfileGameCell>
-            <ProfileGameCell>
+            <ProfileGameCell href={href}>
                 {props.status === 'DRAW' ? (
                     <><BiSolidMinusSquare className="inline text-primary text-lg mr-1.5" /> 0.5-0.5</>
                 ) : props.status === 'FIRST_WON' && username === props.first.username ? (
@@ -48,17 +44,20 @@ export default function ProfileGame(props: ProfileGameInfo) {
                     <><BiSolidMinusSquare className="inline text-red-600 text-lg mr-1.5" /> 0-1</>
                 )}
             </ProfileGameCell>
-            <ProfileGameCell className="text-right">
+            <ProfileGameCell href={href} className="text-right">
                 {DateTime.fromSQL(props.createdAt).toLocaleString()}
             </ProfileGameCell>
         </div>
     )
 }
 
-function ProfileGameCell(props: {children: ReactNode, className?: string}) {
+function ProfileGameCell(props: {children: ReactNode, href: string, className?: string}) {
     return (
-        <div className={'table-cell px-6 py-3 border-t border-tertiary align-middle' + (props.className ? ` ${props.className}` : '')}>
+        <Link
+            href={props.href}
+            className={'table-cell px-6 py-3 border-t border-tertiary align-middle' + (props.className ? ` ${props.className}` : '')}
+        >
             {props.children}
-        </div>
+        </Link>
     )
 }
