733bce13a884f333221c4e9f1f79d92562cfe20b
Kevin Yu <kevinyu567@gmail.com>
2023-10-22 16:29:42 -0400

    Display last played rating in profile page title

--- app/profile/[id]/page.tsx
+++ app/profile/[id]/page.tsx
@@ -14,10 +14,17 @@ export async function generateMetadata({ params }: { params: { id: string } }): 
     const joinedAtDate = DateTime.fromSQL(user.createdAt);
     const games = Object.values(user.perfs).reduce((sum, perf) => sum + perf.games, 0);
 
-    // TODO: rating
+    // Display rating for the game type of the user's last rated game.
+    // TODO: see TODO in `ProfileGames.tsx`
+    const ratedGames = user.games
+        .filter((g) => g.rated)
+        .sort((gameA, gameB) => DateTime.fromSQL(gameA.createdAt).valueOf() - DateTime.fromSQL(gameB.createdAt).valueOf());
+    const lastRatedGameInfo = ratedGames[ratedGames.length - 1].game;
+    const rating = Math.floor(user.perfs[lastRatedGameInfo.key].rating);
+
     return {
-        title: `${params.id} (1337)`,
-        description: `${params.id} joined on ${joinedAtDate.toLocaleString()} and has played ${games} games.` // TODO: rating?
+        title: `${params.id} (${rating})`,
+        description: `${params.id} joined on ${joinedAtDate.toLocaleString()} and has played ${games} games. Current ${lastRatedGameInfo.name} rating: ${rating}.`
     }
 }
 
