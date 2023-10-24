fead38d9128d6142d76db4f454cb74d31a7529ac
Kevin Yu <kevinyu567@gmail.com>
2023-10-23 22:29:26 -0400

    Update deps again, hopefully fix highcharts SSR type error
    And hopefully this doesn't come at the cost of a "flash of no elo chart" on profile reload!

--- app/profile/ProfileEloChart.tsx
+++ app/profile/ProfileEloChart.tsx
@@ -18,175 +18,174 @@ import type {GameKey} from '../../contexts/ProfileContext';
 if (typeof Highcharts === 'object') {
     HighchartsAccessibility(Highcharts);
     HighchartsExporting(Highcharts);
-}
 
+    const text = {
+        weak: '#707070',
+        strong: '#a0a0a0'
+    }
 
-const text = {
-    weak: '#707070',
-    strong: '#a0a0a0'
-}
+    const line = {
+        weak: '#404040',
+        strong: '#606060',
+        accent: '#d85000',
+        white: '#ffffff',
+        black: '#333333',
+        grey: '#777777',
+    }
 
-const line = {
-    weak: '#404040',
-    strong: '#606060',
-    accent: '#d85000',
-    white: '#ffffff',
-    black: '#333333',
-    grey: '#777777',
-}
-
-Highcharts.setOptions({
-    chart: {
-        // @ts-ignore
-        backgroundColor: null,
-        borderWidth: 0,
-        borderRadius: 0,
-        // @ts-ignore
-        plotBackgroundColor: null,
-        plotShadow: false,
-        plotBorderWidth: 0,
-    },
-    title: {
-        style: {
-            font: '13px',
-            color: text.strong,
-        }
-    },
-    xAxis: {
-        gridLineWidth: 0,
-        gridLineColor: line.weak,
-        lineColor: line.strong,
-        tickColor: line.strong,
-        labels: {
-            style: {
-                color: text.weak,
-                fontWeight: 'bold',
-            },
+    Highcharts.setOptions({
+        chart: {
+            // @ts-ignore
+            backgroundColor: null,
+            borderWidth: 0,
+            borderRadius: 0,
+            // @ts-ignore
+            plotBackgroundColor: null,
+            plotShadow: false,
+            plotBorderWidth: 0,
         },
         title: {
             style: {
-                color: text.weak,
-                font: '12px',
-            },
-        },
-        crosshair: { color: line.weak }
-    },
-    // @ts-ignore
-    yAxis: {
-        alternateGridColor: null,
-        minorTickInterval: null,
-        gridLineColor: line.weak,
-        minorGridLineColor: null,
-        lineWidth: 0,
-        tickWidth: 0,
-        labels: {
-            style: {
-                color: text.weak,
-                fontSize: '10px',
+                font: '13px',
+                color: text.strong,
             }
-        },
-        title: {
-            style: {
-                color: text.weak,
-                font: '12px',
-            }
-        },
-    },
-    legend: {
-        itemStyle: { color: text.strong },
-        itemHiddenStyle: { color: text.weak }
-    },
-    /*
-    labels: {
-        style: {
-            color: text.strong,
-        },
-    },
-    */
-    lang: {
-        thousandsSep: '',
-    },
-    tooltip: {
-        backgroundColor: {
-            linearGradient: {
-                x1: 0,
-                y1: 0,
-                x2: 0,
-                y2: 1,
-            },
-            stops: [
-                [0, 'rgba(56, 56, 56, .8)'],
-                [1, 'rgba(16, 16, 16, .8)'],
-            ],
-        },
-        borderWidth: 0,
-        style: {
-            fontWeight: 'bold',
-            color: text.strong
-        }
-    },
-    plotOptions: {
-        series: {
-            shadow: false,
-            // nullColor: '#444444',
-        },
-        line: {
-            dataLabels: { color: text.strong },
-            marker: { lineColor: text.weak }
-        },
-        spline: {
-            marker: { lineColor: text.weak }
-        },
-        scatter: {
-            marker: { lineColor: text.weak }
-        },
-        candlestick: { lineColor: text.strong }
-    },
-
-    rangeSelector: {
-        buttonTheme: {
-            fill: '#505053',
-            stroke: '#000000',
-            style: { color: '#CCC' },
-            states: {
-                hover: {
-                    fill: '#707073',
-                    stroke: '#000000',
-                    style: { color: 'white' }
-                },
-                select: {
-                    fill: '#000003',
-                    stroke: '#000000',
-                    style: { color: 'white' },
-                },
-            },
-        },
-        inputBoxBorderColor: '#505053',
-        inputStyle: {
-            backgroundColor: '#333',
-            color: 'silver',
-        },
-        labelStyle: {
-            color: 'silver',
-        },
-    },
-
-    navigator: {
-        handles: {
-            backgroundColor: '#666',
-            borderColor: '#AAA',
-        },
-        outlineColor: '#CCC',
-        maskFill: 'rgba(255, 255, 255, 0.1)',
-        series: {
-            color: '#7798BF',
-            lineColor: '#A6C7ED',
         },
         xAxis: {
-            gridLineColor: '#505053',
+            gridLineWidth: 0,
+            gridLineColor: line.weak,
+            lineColor: line.strong,
+            tickColor: line.strong,
+            labels: {
+                style: {
+                    color: text.weak,
+                    fontWeight: 'bold',
+                },
+            },
+            title: {
+                style: {
+                    color: text.weak,
+                    font: '12px',
+                },
+            },
+            crosshair: { color: line.weak }
         },
-    }
-})
+        // @ts-ignore
+        yAxis: {
+            alternateGridColor: null,
+            minorTickInterval: null,
+            gridLineColor: line.weak,
+            minorGridLineColor: null,
+            lineWidth: 0,
+            tickWidth: 0,
+            labels: {
+                style: {
+                    color: text.weak,
+                    fontSize: '10px',
+                }
+            },
+            title: {
+                style: {
+                    color: text.weak,
+                    font: '12px',
+                }
+            },
+        },
+        legend: {
+            itemStyle: { color: text.strong },
+            itemHiddenStyle: { color: text.weak }
+        },
+        /*
+        labels: {
+            style: {
+                color: text.strong,
+            },
+        },
+        */
+        lang: {
+            thousandsSep: '',
+        },
+        tooltip: {
+            backgroundColor: {
+                linearGradient: {
+                    x1: 0,
+                    y1: 0,
+                    x2: 0,
+                    y2: 1,
+                },
+                stops: [
+                    [0, 'rgba(56, 56, 56, .8)'],
+                    [1, 'rgba(16, 16, 16, .8)'],
+                ],
+            },
+            borderWidth: 0,
+            style: {
+                fontWeight: 'bold',
+                color: text.strong
+            }
+        },
+        plotOptions: {
+            series: {
+                shadow: false,
+                // nullColor: '#444444',
+            },
+            line: {
+                dataLabels: { color: text.strong },
+                marker: { lineColor: text.weak }
+            },
+            spline: {
+                marker: { lineColor: text.weak }
+            },
+            scatter: {
+                marker: { lineColor: text.weak }
+            },
+            candlestick: { lineColor: text.strong }
+        },
+
+        rangeSelector: {
+            buttonTheme: {
+                fill: '#505053',
+                stroke: '#000000',
+                style: { color: '#CCC' },
+                states: {
+                    hover: {
+                        fill: '#707073',
+                        stroke: '#000000',
+                        style: { color: 'white' }
+                    },
+                    select: {
+                        fill: '#000003',
+                        stroke: '#000000',
+                        style: { color: 'white' },
+                    },
+                },
+            },
+            inputBoxBorderColor: '#505053',
+            inputStyle: {
+                backgroundColor: '#333',
+                color: 'silver',
+            },
+            labelStyle: {
+                color: 'silver',
+            },
+        },
+
+        navigator: {
+            handles: {
+                backgroundColor: '#666',
+                borderColor: '#AAA',
+            },
+            outlineColor: '#CCC',
+            maskFill: 'rgba(255, 255, 255, 0.1)',
+            series: {
+                color: '#7798BF',
+                lineColor: '#A6C7ED',
+            },
+            xAxis: {
+                gridLineColor: '#505053',
+            },
+        }
+    })
+}
 
 
 type ProfileEloChartProps = {games: GameInfo[]}
